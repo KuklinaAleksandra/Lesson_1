c16abe5b939ba1ec9bd608fcfde7a0aaff49b8c8
Aleksandra <kuklina.aleksandra@rambler.ru>
2021-03-29 20:57:28 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как Вас зовут? ");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Здравствуй, {name}, сегодня [{DateTime.Now}]");
+           
+        }
+        
+    }
+}
